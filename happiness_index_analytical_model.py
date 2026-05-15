fix bug in cost calculation in analytical model and rename file name to bring clarity

happiness_index_analytical_model.py
@@ -6,28 +6,28 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 
-# Load CSV file
 df = pd.read_csv("Univariate Linear Regression\\gdp-vs-happiness.csv")
 
-# Select columns
 x_train = df['GDP per capita'].to_numpy()
 y_train = df['Life satisfaction'].astype(float).to_numpy()
 # x_train = np.array([2, 4, 6, 8, 10])
 # y_train = np.array([5, 9, 12, 15, 20])
 
-slope = lrp.get_slope(x_train,y_train)
-intercept = lrp.get_intercept(slope,x_train,y_train)
-cost = cost.calculate_cost(x_train,y_train,slope,intercept)
+slope = lrp.get_slope(x_train, y_train)
+print("Slope is :", slope)
+intercept = lrp.get_intercept(slope, x_train, y_train)
+cost = cost.calculate_cost(x_train, y_train, intercept, slope)
 print(f"Cost function evaluates to {cost:.2f}")
 
-y_hat = lrp.calculate_predicted_values(slope, intercept,x_train,y_train)
+y_hat = lrp.calculate_predicted_values(slope, intercept, x_train, y_train)
 
-print(f"Best fit line equation is : y(hat) = {intercept:.1f} + {slope:.8f} x_i")
+print(
+    f"Best fit line equation is : y(hat) = {intercept:.1f} + {slope:.8f} x_i")
 
 print("Predict the happiness index of country 'Cyprus' having a GDP per capita of 37655")
 x_test = 37655
-prediction=intercept+(slope) * x_test
-print("Happiness Index is" , prediction)
+prediction = intercept+(slope) * x_test
+print("Happiness Index is", prediction)
 
 plt.scatter(x_train, y_train, marker="X", c="r", label='Actual Values')
 plt.plot(x_train, y_hat, marker="o", c="b", label='Predicted Values')

linear_regression_parameters.py
@@ -1,49 +1,54 @@
 import numpy as np
 
+
 def mean(arr):
-    sum=0
+    sum = 0
     for ele in arr:
-        sum=sum+ele
+        sum = sum+ele
     return (sum/len(arr))
 
 
 def mean_difference(arr, mean):
-    diff=np.zeros(len(arr))
+    diff = np.zeros(len(arr))
     for i in range(len(arr)):
         diff[i] = arr[i] - mean
     return diff
-    
 
-def muliply_mean_differences_and_sum(arrX,arrY):
-    result=0
+
+def muliply_mean_differences_and_sum(arrX, arrY):
+    result = 0
     for i in range(len(arrX)):
-        result=result + (arrX[i]*arrY[i])
+        result = result + (arrX[i]*arrY[i])
     return result
 
-def mean_diff_square(arr):
-    mean_sq=0
+
+def mean_diff_square_sum(arr):
+    mean_sq = 0
     for i in range(len(arr)):
-         mean_sq=mean_sq+(arr[i] * arr[i])
+        mean_sq = mean_sq + (arr[i] * arr[i])
     return mean_sq
 
-def get_slope(x_train,y_train):
+
+def get_slope(x_train, y_train):
     x_mean = mean(x_train)
+    print("Mean of X is :", x_mean)
     y_mean = mean(y_train)
+    print("Mean of Y is :", y_mean)
     x_mean_diff = mean_difference(x_train, x_mean)
     y_mean_diff = mean_difference(y_train, y_mean)
     x_y_mean_difference_product_sum = muliply_mean_differences_and_sum(
         x_mean_diff, y_mean_diff)
-    x_mean_difference_square = mean_diff_square(x_mean_diff)
+    x_mean_difference_square = mean_diff_square_sum(x_mean_diff)
     return x_y_mean_difference_product_sum/x_mean_difference_square
 
 
-def get_intercept(slope,x_train,y_train):
+def get_intercept(slope, x_train, y_train):
     x_mean = mean(x_train)
     y_mean = mean(y_train)
     return y_mean-(slope)*x_mean
 
 
-def calculate_predicted_values(slope, intercept,x_train,y_train):
+def calculate_predicted_values(slope, intercept, x_train, y_train):
     y_hat = np.zeros(len(x_train))
     for i in range(len(x_train)):
         y_hat[i] = intercept+(slope) * x_train[i]