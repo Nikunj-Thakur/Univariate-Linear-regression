3bcffb0b51739c44e11f632a15ad44749e621331
Nikunj Thakur <nik.thak22@gmail.com>
2026-05-25 15:36:26 +0100

    update axis labels

--- happiness_index_analytical_model.py
+++ happiness_index_analytical_model.py
@@ -9,10 +9,13 @@ plt.style.use('ggplot')
 df = pd.read_csv("Univariate Linear Regression\\gdp-vs-happiness.csv")
 
 x_train = df['GDP per capita'].to_numpy()
+y_train = df['Life satisfaction'].astype(float).to_numpy()
+
+# Scaling the feture using Z-score standardisation to prevent overflow
 x_mean = x_train.mean()
 x_std = x_train.std()
 x_train = (x_train - x_mean) / x_std
-y_train = df['Life satisfaction'].astype(float).to_numpy()
+
 # x_train = np.array([2, 4, 6, 8, 10])
 # y_train = np.array([5, 9, 12, 15, 20])
 
@@ -34,7 +37,7 @@ print("Happiness Index is", f"{prediction:0.2f}")
 
 plt.scatter(x_train, y_train, marker="X", c="r", label='Actual Values')
 plt.plot(x_train, y_hat, marker="o", c="b", label='Predicted Values')
-plt.xlabel("GDP per capita")
+plt.xlabel("GDP per capita, standardised values")
 plt.ylabel("Life satisfaction")
 plt.title("Simple Linear Regression Model")
 plt.legend()
